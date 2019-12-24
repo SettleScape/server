3d11f3017032de8e8c7f5595b156ec6bf3d288a1
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2019-12-24 15:54:43 -0500

    Optimized JAVA_OPTS

--- start.sh
+++ start.sh
@@ -1,6 +1,54 @@
 #!/usr/bin/env sh
-JAVA_OPTS='-Xms1G -Xmx1G -XX:+UseConcMarkSweepGC'
 SERVER_JAR='./server.jar'
+
+## Official 1.14.4 client options.
+# VANILLA_OPTS=$(echo                    \
+#     '-Xmx2G'                           \
+#     '-XX:+UnlockExperimentalVMOptions' \
+#     '-XX:+UseG1GC'                     \
+#     '-XX:G1NewSizePercent=20'          \
+#     '-XX:G1ReservePercent=20'          \
+#     '-XX:MaxGCPauseMillis=50'          \
+#     '-XX:G1HeapRegionSize=32M'         \
+# | xargs)
+
+## Aikar's tuned server options.
+## https://aikar.co/2018/07/02/tuning-the-jvm-g1gc-garbage-collector-flags-for-minecraft
+# AIKAR_OPTS=$(echo                          \
+#     '-Xms6G'                               \
+#     '-Xmx6G'                               \
+#     '-XX:+UnlockExperimentalVMOptions'     \
+#     '-XX:+AlwaysPreTouch'                  \
+#     '-XX:+ParallelRefProcEnabled'          \
+#     '-XX:+DisableExplicitGC'               \
+#     '-XX:+UseG1GC'                         \
+#     '-XX:G1MixedGCLiveThresholdPercent=35' \
+#     '-XX:G1NewSizePercent=50'              \
+#     '-XX:G1MaxNewSizePercent=80'           \
+#     '-XX:TargetSurvivorRatio=90'           \
+#     '-XX:MaxGCPauseMillis=100'             \
+#     '-Dusing.aikars.flags=mcflags.emc.gs'  \
+# | xargs)
+
+## Mixes the above options.
+## The percentages in these flags have been changed from their original values to better-align with duodecimal percentages.
+JAVA_OPTS=$(echo                           \
+    '-Xmx2G'                               \
+    '-XX:+UnlockExperimentalVMOptions'     \
+    '-XX:+AlwaysPreTouch'                  \
+    '-XX:+ParallelRefProcEnabled'          \
+    '-XX:+DisableExplicitGC'               \
+    '-XX:+UseG1GC'                         \
+    '-XX:G1HeapRegionSize=32M'             \
+    '-XX:G1ReservePercent=25'              \
+    '-XX:G1MixedGCLiveThresholdPercent=33' \
+    '-XX:G1NewSizePercent=50'              \
+    '-XX:G1MaxNewSizePercent=75'           \
+    '-XX:TargetSurvivorRatio=92'           \
+    '-XX:MaxGCPauseMillis=50'              \
+    '-Dusing.aikars.flags=mcflags.emc.gs'  \
+| xargs)
+# echo $JAVA_OPTS && exit
 
 exec screen -d -m -S "Minecraft Server" \
 java $JAVA_OPTS -jar "$SERVER_JAR"
