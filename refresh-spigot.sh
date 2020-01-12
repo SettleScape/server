0b8af74da554e3062ebf5c5508d02c461205670b
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2020-01-12 18:58:08 -0500

    Unified server update scripts

--- refresh-spigot.sh
+++ refresh-spigot.sh
@@ -2,20 +2,40 @@
 set -e
 
 ## Variables
-BUILDDIR='build'
-BUILDJAR='BuildTools.jar'
-  OUTJAR='server.jar'
- VERSION='1.14.4'
+   TYPE='paper'
+ OUTJAR='server.jar'
+VERSION='1.14.4'
 
-## Prepare build directory
-rm    -rf "$BUILDDIR" 2> /dev/null
-mkdir -p  "$BUILDDIR" 2> /dev/null
-cd        "$BUILDDIR"
+## Do the thing
+case "$TYPE" in
 
-## Perform the build
-curl -o "$BUILDJAR" 'https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar'
-java -jar "$BUILDJAR" --rev "$VERSION"
+    'bukkit')
+        #TODO
+    ;;
 
-## Deploy the new JAR
-mv spigot*.jar "../$OUTJAR"
+    'spigot')
+
+        ## Variables
+        BUILDDIR='build'
+        BUILDJAR='BuildTools.jar'
+
+        ## Prepare build directory
+        rm    -rf "$BUILDDIR" 2> /dev/null
+        mkdir -p  "$BUILDDIR" 2> /dev/null
+        cd        "$BUILDDIR"
+
+        ## Perform the build
+        curl -o "$BUILDJAR" 'https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar'
+        java -jar "$BUILDJAR" --rev "$VERSION"
+
+        ## Deploy the new JAR
+        mv spigot*.jar "../$OUTJAR"
+    ;;
+
+    'paper')
+        curl -o "$OUTJAR" "https://papermc.io/api/v1/paper/${VERSION}/latest/download"
+    ;;
+esac
+
+## Cleanup
 exit 0
