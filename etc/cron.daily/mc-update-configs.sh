2db126911b629f3e996af5dfd10c4422033d8627
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2020-01-12 19:02:53 -0500

    Added a script to automatically update some plugins

--- etc/cron.daily/mc-update-configs.sh
+++ etc/cron.daily/mc-update-configs.sh
@@ -4,4 +4,7 @@ cd /srv/minecraft
 echo 'Updating SettleScape...'
 cd settlescape
 sudo -u minecraft git pull
+sudo -u minecraft sh 'refresh-server.sh' &
+sudo -u minecraft sh 'refresh-plugins.sh' &
+wait
 cd ..
