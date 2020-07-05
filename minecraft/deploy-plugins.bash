877244a2902bd7cf0ae23d4d0089b1b88473e5d3
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2020-07-05 19:44:16 -0400

    The deploy plugins script required bashisms

--- minecraft/deploy-plugins.bash
+++ minecraft/deploy-plugins.bash
@@ -1,10 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+## This script requires the [[ double-brackets ]] of `bash` in order to run;
+## so a conversion to POSIX `sh` is not straightforward.
 set -e
 
 ## Handle paths
 CWD=$(pwd)
 TWD='plugins'
-[ ! "$CWD" = *"/$TWD" ] && cd "./$TWD"
+[[ ! "$CWD" == *"/$TWD" ]] && cd "./$TWD"
 
 ## Handle input
 # INPUT=$@
@@ -13,7 +15,7 @@ INPUT=$(ls -A1 | grep '\.jar$')
 
 ## Send specified plugins to the server
 for PLUGIN in $INPUT; do
-    [ ! "$PLUGIN" = *'.jar' ] && PLUGIN="${PLUGIN}.jar"
+    [[ ! "$PLUGIN" == *'.jar' ]] && PLUGIN="${PLUGIN}.jar"
     scp "$PLUGIN" "minecraft@settlescape:/srv/minecraft/settlescape/$TWD"
 done
 
