b78f4edb9b2b2a11ab4d3d48df81fe60ae287db0
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2020-08-01 23:35:29 -0400

    Added a flag to facilite noninteractive updates

--- etc/cron.daily/auto-update.sh
+++ etc/cron.daily/auto-update.sh
@@ -4,7 +4,7 @@ set -e
     apt-get update &&\
     apt-get dist-upgrade
 elif [[ ! -z "$(which 'yum')"    ]] && [[ "$(which 'yum')"    != *'not found'* ]]; then  ## yum next because RHEL is widespread
-    yum update
+    yum update -y
 elif [[ ! -z "$(which 'zypper')" ]] && [[ "$(which 'zypper')" != *'not found'* ]]; then  ## then zypper, because OpenSuSE is less widespread than RHEL-ish distros, but still mainstream...ish.
 	zypper update
   fi
