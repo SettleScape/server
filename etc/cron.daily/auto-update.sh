f2c3f0740280ffb3238794f29d2432180e2954b0
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2020-08-01 20:46:00 -0400

    `auto-update.sh` now supports multiple distros.

--- etc/cron.daily/auto-update.sh
+++ etc/cron.daily/auto-update.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 set -e
-apt-get update &&\
-apt-get dist-upgrade
+  if [[ ! -z "$(which 'apt')"    ]] && [[ "$(which 'apt')"    != *'not found'* ]]; then  ## aptitude etc come first because Debian-derivatives are EVERYWHERE
+    apt-get update &&\
+    apt-get dist-upgrade
+elif [[ ! -z "$(which 'yum')"    ]] && [[ "$(which 'yum')"    != *'not found'* ]]; then  ## yum next because RHEL is widespread
+    yum update
+elif [[ ! -z "$(which 'zypper')" ]] && [[ "$(which 'zypper')" != *'not found'* ]]; then  ## then zypper, because OpenSuSE is less widespread than RHEL-ish distros, but still mainstream...ish.
+	zypper update
+  fi
