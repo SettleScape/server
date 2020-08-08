caead2a933278ff04ec7ac3e8a65cc91d9b53136
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2020-08-08 17:15:38 -0400

    Added a script to enforce correct permissions

---
+++ minecraft/scripts/fix-perms.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+## This script ensures that the `minecraft` folder is permissioned correctly.
+. './env.sh'
+
+## Go to the right directory
+cd "$ENV_MC_HOME"
+
+## Do the thing
+chmod -Rc 'u+rw'      '.'
+chown -Rc 'minecraft' '.'
+chmod -Rc 'g+rw'      '.'
+chgrp -Rc 'minecraft' '.'
+chmod -Rc 'o-rw'      '.'
+
+## Done
+exit 0
