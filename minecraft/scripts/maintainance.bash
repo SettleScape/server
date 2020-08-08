e6db3f5dfed440006792588887811f61eadb88e8
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2020-08-08 17:15:54 -0400

    Added a script to perform daily, in-game maintenance

---
+++ minecraft/scripts/maintainance.bash
@@ -0,0 +1,19 @@
+#!/bin/sh
+## This script sends certain commands to a running Minecraft `screen` instance.
+## It is used to perform daily maintenance tasks from within Minecraft itself.
+source './env.sh'
+
+## Configure the stuff command
+STUFF="screen -S "$ENV_SCREEN_NAME" -X stuff"
+
+## Clean CoreProtect database
+eval "$STUFF 'co purge t:15d \n'" && sleep 5
+## (Head moderator says 2 weeks is a good statute of limitations.)
+
+## Fix splotchy maps
+eval "$STUFF 'dynmap fullrender world \n'"         && sleep 1
+eval "$STUFF 'dynmap fullrender world_nether \n'"  && sleep 1
+eval "$STUFF 'dynmap fullrender world_the_end \n'" && sleep 1
+
+## Done
+exit 0
--- notes/daily-commands.rst
+++
@@ -1,8 +0,0 @@
-To fix splotchy maps:
-- dynmap fullrender world
-- dynmap fullrender world_nether
-- dynmap fullrender world_the_end
-
-To clean CoreProtect database:
-- co purge t:15d
-(Head moderator says 2 weeks is a good statute of limitations.)
