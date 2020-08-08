283dce8c0bc3adb96ad3db1e82f360ebe0895576
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2020-08-08 19:09:23 -0400

    Added `echo`s to increase friendliness

--- minecraft/scripts/maintainance.bash
+++ minecraft/scripts/maintainance.bash
@@ -6,14 +6,15 @@ source './env.sh'
 ## Configure the stuff command
 STUFF="screen -S "$ENV_SCREEN_NAME" -X stuff"
 
-## Clean CoreProtect database
+echo 'Cleaning CoreProtect database...'
 eval "$STUFF 'co purge t:15d \n'" && sleep 5
 ## (Head moderator says 2 weeks is a good statute of limitations.)
 
-## Fix splotchy maps
+echo 'Fixing splotchy maps...'
 eval "$STUFF 'dynmap fullrender world \n'"         && sleep 1
 eval "$STUFF 'dynmap fullrender world_nether \n'"  && sleep 1
 eval "$STUFF 'dynmap fullrender world_the_end \n'" && sleep 1
 
 ## Done
+echo 'Done.'
 exit 0
