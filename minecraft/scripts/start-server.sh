0658093f40539fe0d48b63ebbedb0b947acf3e8a
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2020-10-18 13:30:56 -0400

    Added a space

--- minecraft/scripts/start-server.sh
+++ minecraft/scripts/start-server.sh
@@ -59,7 +59,7 @@ cd "$ENV_SERVER_ROOT"
 screen -d -m -S "$ENV_SCREEN_NAME" \
 "$JAVA" $JAVA_OPTS -jar "./$ENV_SERVER_JAR" --nogui #--forceUpgrade
 #NOTE: Type `screen -r 'SettleScape'` to attach to the SettleScape screen.
-#NOTE: Press Ctrl+A,Ctrl+D to detatch from the SettleScape screen.
+#NOTE: Press Ctrl+A, Ctrl+D to detatch from the SettleScape screen.
 #NOTE: Type `screen -S 'SettleScape' -X stuff "$COMMAND\n"` to send a command to the SettleScape screen.
 screen -r "$ENV_SCREEN_NAME"
 exit 0
