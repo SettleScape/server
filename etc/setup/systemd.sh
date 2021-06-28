12a59c5b9700362f4aa30f265cba6779907fbb66
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2021-06-27 20:29:08 -0400

    Some fixes to the last commit

--- etc/setup/systemd.sh
+++ etc/setup/systemd.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo loginctl enable-linger minecraft
 sudo --su minecraft mkdir -p /srv/minecraft/.config/systemd/user
-sudo cp -a /srv/minecraft/settlescape/etc/systemd/system/settlescape.service /srv/minecraft/.config/systemd/user/
+sudo ln -s /srv/minecraft/settlescape/etc/systemd/system/settlescape.service /srv/minecraft/.config/systemd/user/
 sudo --su minecraft systemctl --user enable settlescape.service
