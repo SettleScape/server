a863a2f4739370f097f8ba71fe743d07dc2cb7ec
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2022-01-09 19:15:44 -0500

    Fix path

--- etc/cron.daily/mc-update-configs.sh
+++ etc/cron.daily/mc-update-configs.sh
@@ -6,7 +6,7 @@ cd settlescape
 sudo -u minecraft git pull
 ## Update Paper
 cd minecraft/scripts
-sudo -u minecraft sh 'refresh-server.sh'
+sudo -u minecraft sh 'update-server.sh'
 ## Cleanup
 cd "$CWD"
 exit 0
