632d68690658ad3927a4f9fce5323ceb59bce690
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2022-01-30 20:36:30 -0500

    Added a reboot script

---
+++ etc/cron.daily/reboot.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+source '/srv/minecraft/settlescape/minecraft/scripts/env.sh'
+STUFF="screen -S "$ENV_SCREEN_NAME" -X stuff"
+
+I=600
+while [ $I -gt 0 ]; do
+    case
+        600) 300) 120) 60) 30) 15) 10) 9) 8) 7) 6) 5) 4) 3) 2) 1)
+        eval "$STUFF 'say Automatic shutdown in $I seconds.'"
+        ;;
+    esac
+    sleep 1
+    I=`expr I+1`
+done
+
+systemctl reboot -i
+exit 0
