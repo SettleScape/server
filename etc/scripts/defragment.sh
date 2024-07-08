597718ceab24db79c1544a457bd0eaa269e45193
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2024-07-08 08:15:58 -0400

    ChatGPT helped fill it out real quick

--- etc/scripts/defragment.sh
+++ etc/scripts/defragment.sh
@@ -4,28 +4,42 @@ set -e
 
 ## Configurable variables:
 FRAGMENTATION_THRESHOLD=10
+LOGFILE='/var/log/defragmentation.log'
 
 ## Find all filesystems that are mounted and on an HDD.
 declare -a MOUNTED_HDD_DEVICES=()
-LSBLK_ENTRIES=$(lsblk -d -o 'name,mountpoint,rota' 2>/dev/null) #TODO: Remove the first line of the output
-for ENTRY in "$LSBLK_ENTRIES"; do
-    #TODO: Use awk to get the third field.  If 0, continue.
-    #TODO: Use awk to get the second field.  If empty, continue.
-    #TODO: Use awk to get the first field, and save it in MOUNTED_HDD_DEVICES.
-done
+LSBLK_ENTRIES=$(lsblk -pro 'NAME,ROTA,MOUNTPOINT' 2>/dev/null | tail -n +2)
+while IFS= read -r ENTRY; do
+
+    MOUNTPOINT=$(echo "$ENTRY" | awk '{print $3}')
+    [[ -z "$MOUNTPOINT" ]] && continue
+
+    ROTA=$(echo "$ENTRY" | awk '{print $2}')
+    [[ "$ROTA" -eq 0 ]] && continue
+
+    DEVICE=$(echo "$ENTRY" | awk '{print $1}')
+    MOUNTED_HDD_DEVICES+=("$DEVICE")
+
+done <<< "$LSBLK_ENTRIES"
 unset LSBLK_ENTRIES
 
 ## For each HDD, check the fragmentation level.  If it is above the threshold, save it for further operation.
 declare -a FRAGMENTED_MOUNTED_HDD_DEVICES=()
 for DEVICE in "${MOUNTED_HDD_DEVICES[@]}"; do
-    e4defrag -c "$DEVICE" #TODO: Isolate the fragmentation percentage from this output.  If it is equal to or above FRAGMENTATION_THRESHOLD, save the device in FRAGMENTED_MOUNTED_HDD_DEVICES.
+
+    FRAG_PERCENT=$(e4defrag -c "$DEVICE" | grep -oP '\d+(?=% fragmented)' | head -n 1)
+    if [ "$FRAG_PERCENT" -ge "$FRAGMENTATION_THRESHOLD" ]; then
+        FRAGMENTED_MOUNTED_HDD_DEVICES+=("$DEVICE")
+    fi
+
 done
 unset MOUNTED_HDD_DEVICES
 
 ## For each fragmented HDD, defragment asynchronously.
 for DEVICE in "${FRAGMENTED_MOUNTED_HDD_DEVICES[@]}"; do
-    e4defrag "$DEVICE" #TODO: Save errors to a log file.
+    e4defrag "$DEVICE" 2>> "$LOGFILE" &
 done
+wait
 unset FRAGMENTED_MOUNTED_HDD_DEVICES
 
 ## Done.
