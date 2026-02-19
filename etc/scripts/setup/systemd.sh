9fec42a4a9fe2124e25a1a6292f0ac20aa598c3f
Miles B Huff <MilesBHuff@users.noreply.github.com>
2026-02-18 19:35:12 -0500

    Added support for dnf-automatic.

--- etc/scripts/setup/systemd.sh
+++ etc/scripts/setup/systemd.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+
 sudo loginctl enable-linger minecraft
 sudo --su minecraft mkdir -p /srv/minecraft/.config/systemd/user
 sudo ln -s /srv/minecraft/settlescape/etc/systemd/system/settlescape.service /srv/minecraft/.config/systemd/user/
 sudo --su minecraft systemctl --user enable settlescape.service
+
+sudo dnf install -y dnf-automatic
+sudo cp -a /srv/minecraft/settlescape/etc/systemd/system/dnf-automatic.timer.d /etc/systemd/system/
+sudo daemon-reload
+sudo systemctl enable dnf-automatic.timer
