6f08003f56433cf10f4dc0fd8425d2d6421359c0
Miles B Huff <MilesBHuff@users.noreply.github.com>
2026-02-20 15:59:39 -0500

    z3fold (unsupported) -> zsmalloc (supported)

--- etc/scripts/setup/swap.sh
+++ etc/scripts/setup/swap.sh
@@ -3,7 +3,7 @@
 ## Enable zswap as a hot cache
 grubby --update-kernel=ALL --args="zswap.enabled=1"
 grubby --update-kernel=ALL --args="zswap.max_pool_percent=17"
-grubby --update-kernel=ALL --args="zswap.zpool=z3fold" ## Ignored on Alma 9.
+grubby --update-kernel=ALL --args="zswap.zpool=zsmalloc"
 grubby --update-kernel=ALL --args="zswap.compressor=lz4" ## Ignored on Alma 9.
 
 ## Enable zram swap as a cold cache
