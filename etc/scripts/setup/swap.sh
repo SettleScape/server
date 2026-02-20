d4e09804f6e5bf2dcfab1ce49a690f4adec73d60
Miles B Huff <MilesBHuff@users.noreply.github.com>
2026-02-20 16:10:24 -0500

    Did the legwork for a zram writeback device.

--- etc/scripts/setup/swap.sh
+++ etc/scripts/setup/swap.sh
@@ -8,7 +8,13 @@ grubby --update-kernel=ALL --args="zswap.compressor=lz4" ## Ignored on Alma 9.
 
 ## Enable zram swap as a cold cache
 dnf install -y zram-generator
-cp -a /srv/minecraft/settlescape/etc/systemd/system/zram-generator.conf.d /etc/systemd/system/
+cp -a /srv/minecraft/settlescape/etc/systemd/zram-generator.conf.d /etc/systemd/
 systemctl start dev-zram0.swap
 
+## Enable on-disk swap as a last resort
+fallocate -l 1G /zram0.writeback
+losetup -fP /zram0.writeback
+echo 'Add this to `/etc/fstab`: `/zram0.writeback none loop 0 0`'
+
+## Configure sysctl
 echo 'If your `sysctl.d` files do not already do so, you should have them configure `vm.swappiness=134` and `vm.page-cluster=0`. '
