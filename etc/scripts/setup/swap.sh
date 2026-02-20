1678bcd454e6624f364ae6c6d5105f3f99593ee7
Miles B Huff <MilesBHuff@users.noreply.github.com>
2026-02-20 14:23:50 -0500

    Combined zswap and zram swap into one script.

--- etc/scripts/setup/swap.sh
+++ etc/scripts/setup/swap.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
-## This script configures the kernel commandline.
-## (It must be run as root.)
+
+## Enable zswap as a hot cache
 grubby --update-kernel=ALL --args="zswap.enabled=1"
 grubby --update-kernel=ALL --args="zswap.max_pool_percent=17"
 grubby --update-kernel=ALL --args="zswap.zpool=z3fold" ## Ignored on Alma 9.
 grubby --update-kernel=ALL --args="zswap.compressor=lz4" ## Ignored on Alma 9.
+
+## Enable zram swap as a cold cache
+dnf install -y zram-generator
+systemctl start dev-zram0.swap
+
+echo 'If your `sysctl.d` files do not already do so, you should have them configure `vm.swappiness=134` and `vm.page-cluster=0`. '
--- etc/scripts/setup/zram.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-dnf install -y zram-generator
-systemctl start dev-zram0.swap
