4033b6b95766cd0c04ccbd75c3b41b1b84629056
Miles B Huff <MilesBHuff@users.noreply.github.com>
2026-02-20 16:03:39 -0500

    Added a line to copy the zram config in before enabling it.

--- etc/scripts/setup/swap.sh
+++ etc/scripts/setup/swap.sh
@@ -8,6 +8,7 @@ grubby --update-kernel=ALL --args="zswap.compressor=lz4" ## Ignored on Alma 9.
 
 ## Enable zram swap as a cold cache
 dnf install -y zram-generator
+cp -a /srv/minecraft/settlescape/etc/systemd/system/zram-generator.conf.d /etc/systemd/system/
 systemctl start dev-zram0.swap
 
 echo 'If your `sysctl.d` files do not already do so, you should have them configure `vm.swappiness=134` and `vm.page-cluster=0`. '
