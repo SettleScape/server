58615b3aeffaf4eec97d8234654ef3cd1c7c5004
Miles B Huff <MilesBHuff@users.noreply.github.com>
2026-02-20 12:55:40 -0500

    Audited the JAVA_OPTS.

--- minecraft/scripts/var/java-opts/client.bash
+++ minecraft/scripts/var/java-opts/client.bash
@@ -1,71 +1,49 @@
 #!/bin/bash
-## Configures the JVM.
+## The server's custom JVM options tweaked for use on clients.
+## Please keep comments synchronized with the server options file.
 
-## The server's custom options tweaked for use on clients, with special input from BruceTheMoose's guide.
+## Tweak them for clients
 declare -a CLIENT_JAVA_OPTS=(
     '-XX:+UnlockExperimentalVMOptions'
     '-XX:+UnlockDiagnosticVMOptions'
 
-    ## Memory Settings
-    '-Xms2048M'
-    '-Xmx2048M'
-    '-XX:+AlwaysPreTouch'
-    '-XX:+UseTransparentHugePages'
-    # '-XX:+UseLargePagesInMetaspace' ## Fails when I try it.
-    '-XX:AllocatePrefetchStyle=3' #NOTE: Breaks ZGC and Shenandoah; fine for G1GC.
-    '-XX:InitiatingHeapOccupancyPercent=10'
+    ## System identification
+    '-XX:+NeverActAsServerClassMachine' ## Override default heuristics, just in case they're ever wrong.
+
+    ## Memory initialization
+    '-Xmx2G' ## Set this to some reasonable amount below your system memory.
+    '-Xms2G' ## Always set to the same as -Xmx if you want max performance.
+    # '-XX:+AlwaysPreTouch' ## During initialization, touches everything in the heap. Good hygiene (especially with THP), but slows startup.
+
+    ## Large pages
+    # '-XX:+UseTransparentHugePages' ## Can help a bit, but you need to set `transparent_hugepage.enabled=madvise`, `transparent_hugepage.shmem_enabled=advise`, `transparent_hugepage.defrag=defer+madvise`.
+    # '-XX:+UseLargePages' ## Don't use this; you have to manually allocate a certain number of pages to this purpose, thus reducing RAM generally; and you're depending on Minecraft and the JVM being good at using those. Use THP instead.
+
+    ## Memory vs CPU tradeoffs
+    # '-XX:+UseCompressedOops' ## Enabled automatically if heap <= 32G, but I think it's generally worth enabling always unless you are swimming in memory and constrained on CPU.
+    # '-XX:+UseStringDeduplication' ## Lowers memory consumption by expending CPU. Very system-dependent trade.
 
     ## Misc
-    '-XX:+NeverActAsServerClassMachine'
-    '-XX:+ParallelRefProcEnabled'
-    '-XX:+PerfDisableSharedMem'
-    '-XX:+UseVectorCmov'
-    '-XX:+UseFastUnorderedTimeStamps'
-    '-XX:+UseCriticalJavaThreadPriority'
-    # '-XX:ThreadPriorityPolicy=1' ## Requires `sudo` on Linux.
+    # '-XX:ThreadPriorityPolicy=1' ## Requires `sudo` on Linux, but can be worth trying on Windows.
+    # '-XX:+UseCompactObjectHeaders' ## Requires Java 25, but is basically a free win. #TODO: Enable once you are on Java 25.
 
     ## GC settings
-    '-XX:+DisableExplicitGC'
-    '-XX:GCTimeRatio=50' ## Compromise value;  needs tuning.
-    '-XX:MaxGCPauseMillis=37'
-    '-XX:MaxTenuringThreshold=1'
-    '-XX:SurvivorRatio=32'
-    '-XX:TargetSurvivorRatio=92'
+    '-XX:+DisableExplicitGC' ## This is a defense against bad plugins. Without it, they can cause frequent and noticeable stuttering.
+    # '-XX:+ExplicitGCInvokesConcurrent' ## If you're not using plugins, use this instead of the above.
+    '-XX:MaxGCPauseMillis=75' ## This is a target, not a guarantee. High values are best for performance, but they also cause worse stuttering during collection. Java 17's default is `200`; Minecraft 1.21.11's is `50`, equal to 1 game tick. Servers can afford to go higher than clients, here.
 
     ## G1GC settings
-    '-XX:+UseG1GC'
-    '-XX:G1HeapRegionSize=16M'
-    '-XX:G1HeapWastePercent=18'
-    '-XX:G1MixedGCCountTarget=3'
-    '-XX:G1MixedGCLiveThresholdPercent=90'
-    '-XX:G1ReservePercent=20'
-    '-XX:G1NewSizePercent=23'
-    '-XX:G1MaxNewSizePercent=33'
-    '-XX:G1RSetUpdatingPauseTimePercent=0'
-    '-XX:G1SATBBufferEnqueueingThresholdPercent=30'
-    '-XX:G1ConcMarkStepDurationMillis=5'
-    '-XX:G1ConcRefinementServiceIntervalMillis=150'
-    '-XX:G1ConcRSHotCardLimit=16'
-
-    ## Code optimizations
-    '-XX:-DontCompileHugeMethods'
-    '-XX:MaxNodeLimit=240000'
-    '-XX:NodeLimitFudgeFactor=8000'
-    # '-XX:+UseStringDeduplication' ## Only when memory-constrained
-    '-XX:NmethodSweepActivity=1'
-    '-XX:ReservedCodeCacheSize=400M'
-    '-XX:NonNMethodCodeHeapSize=12M'
-    '-XX:ProfiledCodeHeapSize=194M'
-    '-XX:NonProfiledCodeHeapSize=194M'
-
-    ## GraalVM
-    # '-XX:+EagerJVMCI'
-    # '-Dgraal.LoopRotation=true'
-    # '-Dgraal.TuneInlinerExploration=1'
+    # '-XX:+UseG1GC' ## Java 17 enables this by default, but Minecraft 1.21.11 explicitly sets it anyway.
+    # '-XX:G1NewSizePercent=20' ## Minecraft 1.21.11 ships this value, but it is not optimal; just let Java do its thing.
+    # '-XX:G1ReservePercent=20' ## Minecraft 1.21.11 ships this value, but it is not optimal; just let Java do its thing.
+    # '-XX:G1HeapRegionSize=32M' ## Minecraft 1.21.11 ships this value, but it is not optimal; just let Java do its thing.
 
     ## Security
-    '-Dlog4j2.formatMsgNoLookups=true'
+    '-Dlog4j2.formatMsgNoLookups=true' ## Included out of an abundance of caution since we are still running 1.17.1 and plugins for this version are liable include an unsafe version of log4j. Extremely unlikely, but not impossible.
 )
 
 ## Export to the environment
 export JAVA_OPTS="${CLIENT_JAVA_OPTS[@]}"
+
+## Echo to concole
+echo "$JAVA_OPTS"
--- minecraft/scripts/var/java-opts/server.bash
+++ minecraft/scripts/var/java-opts/server.bash
@@ -1,83 +1,65 @@
 #!/bin/bash
 ## Configures the JVM.
+## Please keep comments synchronized with the client options file.
 
-## PaperMC's suggested options:
+## Be warned: The Internet is full of cool new Java options you can add to your Minecraft server.
+## However, just because something worked well on one person's server 10 years ago is no guarantee that it will work well on yours today.
+## (In particular: most guides are targeted at servers that allocate quite a lot of memory, or at working around design limitations in older versions of Java.)
+## As well: Many of the guides circling around contain conflicting or even non-existent options! (Most of the non-existent ones used to exist on older versions.)
+## You can run the following command to view the defaults and supported options of *your* JVM: `java -XX:+UnlockExperimentalVMOptions -XX:+UnlockDiagnosticVMOptions -XX:+PrintFlagsFinal -version`.
+## Always do this before adding new things, to help ensure you don't specify defaults. It's best to just specify deltas, as that can help reduce how much time must be spent to isolate an issue caused by these options.
+## Unfortunately, there does not appear to be a centralized place documentating what each of these options does. Accordingly, asking an AI is often among the best strategies.
+## One may *think* that the options Mojang ships for the client may be a good starting place, but these are not tuned to be performant — they're apparently tuned to fit the lowest-common-denominator of garbage consumer hardware, all the rest be damned.
+##
+## PaperMC's suggested options: (derived from Aikar's famous flags)
 ## * https://docs.papermc.io/paper/aikars-flags
-##
-## Aikar's server options: (Superceded by PaperMC's guide)
-## * https://docs.papermc.io/paper/aikars-flags
-## * https://aikar.co/2018/07/02/tuning-the-jvm-g1gc-garbage-collector-flags-for-minecraft
-##
-## BruceTheMoose's server options: (Superceded by PaperMC's guide)
-## * https://github.com/Mukul1127/Minecraft-Performance-Flags-Benchmarks
-##
-## JewishLewish's flags:
-## * https://github.com/JewishLewish/minecraft-aikar-flags-optimization/blob/main/README.md
 ##
 ## Overview of transparent huge pages:
-## * https://alexandrnikitin.github.io/blog/transparent-hugepages-measuring-the-performance-impact
+## * https://github.com/Obydux/Minecraft-startup-flags#enabling-transparent-huge-pages-thp-on-linux
 
 ## My personal collection of Java Opts:
 declare -a SERVER_JAVA_OPTS=(
     '-XX:+UnlockExperimentalVMOptions'
     '-XX:+UnlockDiagnosticVMOptions'
 
-    ## Memory Settings
-    '-Xms1536M'
-    '-Xmx1536M'
-    '-XX:+AlwaysPreTouch'
-    '-XX:+UseTransparentHugePages' ## Our `enabled` is set to `madvise` and our `defrag` is set to `defer+madvise`.
-    '-XX:+UseLargePages'
-    '-XX:AllocatePrefetchStyle=3' #NOTE: Breaks ZGC and Shenandoah; fine for G1GC.
-    '-XX:InitiatingHeapOccupancyPercent=15'
-    '-XX:+UseStringDeduplication' ## Trades higher CPU use for reduced memory consumption. We're unfortunately low on both.
+    ## System identification
+    '-XX:+AlwaysActAsServerClassMachine' ## Override default heuristics, just in case they're ever wrong.
+
+    ## Memory initialization
+    '-Xmx1536M' ## Set this to some reasonable amount below your system memory.
+    '-Xms1536M' ## Always set to the same as -Xmx if you want max performance.
+    '-XX:+AlwaysPreTouch' ## During initialization, touches everything in the heap. Good hygiene (especially with THP), but slows startup.
+
+    ## Large pages
+    '-XX:+UseTransparentHugePages' ## Can help a bit, but you need to set `transparent_hugepage.enabled=madvise`, `transparent_hugepage.shmem_enabled=advise`, `transparent_hugepage.defrag=defer+madvise`.
+    # '-XX:+UseLargePages' ## Don't use this; you have to manually allocate a certain number of pages to this purpose, thus reducing RAM generally; and you're depending on Minecraft and the JVM being good at using those. Use THP instead.
+
+    ## Memory vs CPU tradeoffs
+    '-XX:+UseCompressedOops' ## Enabled automatically if heap <= 32G, but I think it's generally worth enabling always unless you are swimming in memory and constrained on CPU.
+    '-XX:+UseStringDeduplication' ## Lowers memory consumption by expending CPU. Very system-dependent trade.
 
     ## Misc
-    '-XX:+AlwaysActAsServerClassMachine'
-    '-XX:+ParallelRefProcEnabled'
-    '-XX:+PerfDisableSharedMem'
     # '-XX:ThreadPriorityPolicy=1' ## Requires `sudo` on Linux, ergo not worth.
-    '-XX:+UseAdaptiveSizePolicy'
-    '-XX:+UseCompressedOops'
-    '-XX:+UseCriticalJavaThreadPriority'
-    '-XX:+UseFastAccessorMethods'
-    '-XX:+UseFastUnorderedTimeStamps'
-    '-XX:+UseTLAB'
-    '-XX:+UseVectorCmov'
+    # '-XX:+UseCompactObjectHeaders' ## Requires Java 25, but is basically a free win. #TODO: Enable once we are on Java 25.
 
     ## GC settings
-    '-XX:+DisableExplicitGC'
-    # '-XX:GCTimeRatio=50' ## This fights `MaxGCPauseMillis`. It is better to set Millis and let Ratio be dynamic.
-    '-XX:MaxGCPauseMillis=100' ## This is a target, not a guarantee. High values are the best for performance, but they also cause worse stuttering during collection. PaperMC suggests 200 (which they got from Aikar), but that is a noticeable amount of stutter; 100 is less-likely to be noticed.
-    '-XX:MaxTenuringThreshold=1'
-    '-XX:SurvivorRatio=33'
-    '-XX:TargetSurvivorRatio=92'
-    '-XX:+UseParNewGC'
-    '-XX:+UseConcMarkSweepGC'
+    '-XX:+DisableExplicitGC' ## This is a defense against bad plugins. Without it, they can cause frequent and noticeable stuttering.
+    '-XX:MaxGCPauseMillis=125' ## This is a target, not a guarantee. High values are best for performance, but they also cause worse stuttering during collection. Java 17's default is `200`; Minecraft 1.21.11's is `50`, equal to 1 game tick. Servers can afford to go higher than clients, here.
 
     ## G1GC settings
-    '-XX:+UseG1GC'
-    '-XX:G1HeapRegionSize=8M'
-    '-XX:G1HeapWastePercent=5'
-    '-XX:G1MixedGCCountTarget=4'
-    '-XX:G1MixedGCLiveThresholdPercent=90'
-    '-XX:G1ReservePercent=20'
-    '-XX:G1NewSizePercent=30'
-    '-XX:G1MaxNewSizePercent=40'
-    '-XX:G1RSetUpdatingPauseTimePercent=5'
-    '-XX:G1SATBBufferEnqueueingThresholdPercent=30'
-    '-XX:G1ConcMarkStepDurationMillis=5'
-    '-XX:G1ConcRefinementServiceIntervalMillis=150'
-    '-XX:G1ConcRSHotCardLimit=16'
+    # '-XX:G1HeapRegionSize=8M' ## Anything >= 50% of this is considered "humongous" and handled suboptimally. Apparently, you want this to be determined automatically (`round(heap / 2048)`) unless the automatic answer is above 8M. Minecraft 1.21.11 ships 32M, which is apparently frankly insane.
 
     ## Reporting
-    '-Dusing.aikars.flags=https://mcflags.emc.gs'
-    '-Daikars.new.flags=true'
-    "-Xlog:gc*,gc+heap=info:file=./gc-logs/$(date +'%Y-%m-%d_%H-%M-%S').log:time,uptime,level,tags:filecount=1,filesize=1M"
+    "-Xlog:gc,gc+heap=info:file=./gc-logs/%p_%t.log:time,uptime,level,tags:filecount=4,filesize=5M"
+    # "-Xlog:gc*,gc+heap=info,gc+age=trace,gc+humongous:file=./gc-logs/%p_%t.log:time,uptime,level,tags:filecount=4,filesize=20M" ## More-verbose option for testing, not for prod.
 
     ## Security
-    '-Dlog4j2.formatMsgNoLookups=true'
-    '-Djdk.tls.ephemeralDHKeySize=2048'
+    '-Dlog4j2.formatMsgNoLookups=true' ## Included out of an abundance of caution since we are still running 1.17.1 and plugins for this version are liable include an unsafe version of log4j. Extremely unlikely, but not impossible.
+
+    ## Warnings
+    # '-XX:AllocatePrefetchStyle=X' ## Don't set this; let it be determined automatically. The best value depends on your CPU, and selecting the wrong one can break things.
+    # '-XX:InitiatingHeapOccupancyPercent=X' ## Do not set this; modern Java adaptively adjusts it up-and-down as needed; see `G1UseAdaptiveIHOP`.
+    # '-XX:GCTimeRatio=X' ## This fights `MaxGCPauseMillis`. It is better to set Millis and let Ratio be dynamic.
 )
 
 ## Export to the environment
